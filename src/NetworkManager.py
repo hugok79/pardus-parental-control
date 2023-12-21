ed692dd5054cf26574560d613863922cd7f47f99
Emin Fedar <eminfedar@gmail.com>
2023-12-21 14:47:21 +0300

    feat: added NetworkManager.py to manage smartdns

---
+++ src/NetworkManager.py
@@ -0,0 +1,69 @@
+import subprocess
+
+SMARTDNS_CONF_PATH = "/etc/smartdns/smartdns.conf"
+
+SMARTDNS_CONF_DEFAULT = """
+# Listen on local port 53
+bind [::]:53
+bind-tcp [::]:53
+
+tcp-idle-time 5
+
+# Cache
+cache-size 32768
+
+# DNS servers, 1.1.1.3 = 1.1.1.1 + No Malware + No Adult
+server 1.1.1.3
+server-tls 1.1.1.3
+
+"""
+
+SMARTDNS_CONF_DENY_DOMAIN_TEMPLATE = """
+address /{}/#
+address /*.{}/#
+"""
+SMARTDNS_CONF_ALLOW_DOMAIN_TEMPLATE = """
+address /{}/-
+"""
+
+SMARTDNS_CONF_ALLOW_DOMAIN_TEMPLATE_END = """
+address /*/#
+"""
+
+
+def reset_dns_settings():
+    with open(SMARTDNS_CONF_PATH, "w") as file1:
+        # Writing data to a file
+        file1.write(SMARTDNS_CONF_DEFAULT)
+
+
+def set_deny_dns_list(list):
+    # example list = ["google.com", "youtube.com"]
+
+    with open(SMARTDNS_CONF_PATH, "w") as file1:
+        content = SMARTDNS_CONF_DEFAULT
+        for domain in list:
+            deny_domain_content = SMARTDNS_CONF_DENY_DOMAIN_TEMPLATE.format(
+                domain)
+            content += "\n" + deny_domain_content
+
+        file1.write(content)
+
+
+def set_allow_dns_list(list):
+    # example list = ["google.com", "youtube.com"]
+
+    with open(SMARTDNS_CONF_PATH, "w") as file1:
+        content = SMARTDNS_CONF_DEFAULT
+        for domain in list:
+            allow_domain_content = SMARTDNS_CONF_ALLOW_DOMAIN_TEMPLATE.format(
+                domain)
+            content += "\n" + allow_domain_content
+
+        content += SMARTDNS_CONF_ALLOW_DOMAIN_TEMPLATE_END
+
+        file1.write(content)
+
+
+def restart_smartdns_service():
+    return subprocess.run(["systemctl", "restart", "smartdns-rs"])
