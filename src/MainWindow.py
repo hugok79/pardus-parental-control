884e01e525f1ba7002dd701235da8395a13df4b2
Emin Fedar <eminfedar@gmail.com>
2024-02-12 14:49:18 +0300

    format: MainWindow.py

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,12 +1,12 @@
 from PreferencesWindow import PreferencesWindow
 from ProfileChooserDialog import ProfileChooserDialog
 import Profiles
-import LinuxUserManager
 
 import os
 import gi
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
 from gi.repository import Gtk, Gdk, Adw  # noqa
 
 CWD = os.path.dirname(os.path.abspath(__file__))
@@ -56,30 +56,32 @@ class MainWindow(Adw.ApplicationWindow):
         css_provider.load_from_path(f"{DATA_DIR}/style.css")
 
         style = self.get_style_context()
-        style.add_provider_for_display(Gdk.Display.get_default(
-        ), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+        style.add_provider_for_display(
+            Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER
+        )
 
     def setup_window(self):
         self.set_default_size(500, 580)
-        self.set_content(Gtk.Box(
-            orientation=Gtk.Orientation.VERTICAL
-        ))
+        self.set_content(Gtk.Box(orientation=Gtk.Orientation.VERTICAL))
         self.content = self.get_content()
 
         self.connect("close-request", lambda x: self.get_application().quit())
 
     def setup_headerbar(self):
         title = Adw.WindowTitle(title="Pardus Parental Control")
-        headerbar = Adw.HeaderBar(
-            title_widget=title,
-            css_classes=["flat"]
-        )
+        headerbar = Adw.HeaderBar(title_widget=title, css_classes=["flat"])
         self.content.append(headerbar)
 
     def setup_main_page(self):
         # Main Page
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, vexpand=True,
-                      spacing=7, margin_top=7, margin_start=7, margin_end=7)
+        box = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL,
+            vexpand=True,
+            spacing=7,
+            margin_top=7,
+            margin_start=7,
+            margin_end=7,
+        )
 
         # Profile and Preferences buttons
         box_top = Gtk.CenterBox()
@@ -90,13 +92,12 @@ class MainWindow(Adw.ApplicationWindow):
             hexpand=False,
             valign="center",
             vexpand=True,
-            css_classes=["success", "flat"]
+            css_classes=["success", "flat"],
         )
         btn_profiles_box = Gtk.Box(spacing=7)
         btn_profiles_box.append(Gtk.Image(icon_name="system-users-symbolic"))
         btn_profiles_box.append(Gtk.Separator(orientation="vertical"))
-        self.lbl_profile_btn = Gtk.Label(
-            label=Profiles.get_current_profile_name())
+        self.lbl_profile_btn = Gtk.Label(label=Profiles.get_current_profile_name())
         btn_profiles_box.append(self.lbl_profile_btn)
         btn_profiles.set_child(btn_profiles_box)
         btn_profiles.connect("clicked", self.on_btn_profiles_clicked)
@@ -106,41 +107,35 @@ class MainWindow(Adw.ApplicationWindow):
             icon_name="open-menu-symbolic",
             halign="end",
             valign="start",
-            css_classes=["flat"]
+            css_classes=["flat"],
         )
 
-        btn_show_preferences.connect(
-            "clicked", self.on_btn_show_preferences_clicked)
+        btn_show_preferences.connect("clicked", self.on_btn_show_preferences_clicked)
         box_top.set_end_widget(btn_show_preferences)
         box_top.set_start_widget(btn_profiles)
 
         box.append(box_top)
 
         # Logo
-        box.append(Gtk.Image(
-            file=f"{DATA_DIR}/img/pardus-parental-control.svg",
-            pixel_size=192,
-            css_classes=["floating"]
-        ))
-        box.append(Gtk.Label(
-            label="Pardus Parental Control",
-            css_classes=["title-2"]
-        ))
+        box.append(
+            Gtk.Image(
+                file=f"{DATA_DIR}/img/pardus-parental-control.svg",
+                pixel_size=192,
+                css_classes=["floating"],
+            )
+        )
+        box.append(Gtk.Label(label="Pardus Parental Control", css_classes=["title-2"]))
 
         # Switch
         switch = Gtk.Switch(
-            halign="center",
-            margin_top=14,
-            margin_bottom=14,
-            css_classes=["bigswitch"]
+            halign="center", margin_top=14, margin_bottom=14, css_classes=["bigswitch"]
         )
         switch.connect("state-set", self.on_switch_activation_state_changed)
         box.append(switch)
 
         # Switch Status
         self.lbl_status = Gtk.Label(
-            label="Parental Protection is Disabled",
-            css_classes=["title-5"]
+            label="Parental Protection is Disabled", css_classes=["title-5"]
         )
         box.append(self.lbl_status)
 
@@ -148,19 +143,20 @@ class MainWindow(Adw.ApplicationWindow):
 
     def setup_dialogs(self):
         self.dialog_preferences = PreferencesWindow(self)
-        self.dialog_profiles = ProfileChooserDialog(
-            self, self.on_profile_selected)
+        self.dialog_profiles = ProfileChooserDialog(self, self.on_profile_selected)
 
     def setup_ui(self):
         self.setup_main_page()
 
         # Copyright
-        self.content.append(Gtk.Label(
-            label="TÜBİTAK ULAKBİM | 2023",
-            valign="end",
-            margin_bottom=7,
-            css_classes=["dim-label"]
-        ))
+        self.content.append(
+            Gtk.Label(
+                label="TÜBİTAK ULAKBİM | 2024",
+                valign="end",
+                margin_bottom=7,
+                css_classes=["dim-label"],
+            )
+        )
 
     # === CALLBACKS ===
     # == Main Window
@@ -172,9 +168,11 @@ class MainWindow(Adw.ApplicationWindow):
 
     def on_switch_activation_state_changed(self, switch, state):
         self.lbl_status.set_label(
-            "Parental Protection Active" if state else "Parental Protection Disabled")
+            "Parental Protection Active" if state else "Parental Protection Disabled"
+        )
         self.lbl_status.set_css_classes(
-            ["title-5", "success"] if state else ["title-5"])
+            ["title-5", "success"] if state else ["title-5"]
+        )
 
     def on_btn_show_preferences_clicked(self, btn):
         self.dialog_preferences.present()
@@ -183,5 +181,4 @@ class MainWindow(Adw.ApplicationWindow):
         self.current_profile = profile_name
         self.lbl_profile_btn.set_label(profile_name)
 
-        self.dialog_preferences.fill_lists_from_profile(
-            Profiles.get_current_profile())
+        self.dialog_preferences.fill_lists_from_profile(Profiles.get_current_profile())
