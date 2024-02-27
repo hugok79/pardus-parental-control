bfa14a28532a40795125757c113e4d777a74ed6b
Emin Fedar <eminfedar@gmail.com>
2024-02-27 11:43:00 +0300

    feat: update Main.py

--- src/Main.py
+++ src/Main.py
@@ -3,16 +3,16 @@ import gi
 import sys
 
 
-gi.require_version('Gtk', '4.0')
+gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gio  # noqa
-from MainWindow import MainWindow  # noqa
+from ui.MainWindow import MainWindow  # noqa
 
 
 class Main(Gtk.Application):
     def __init__(self):
         super().__init__(
             application_id="tr.org.pardus.parental-control",
-            flags=Gio.ApplicationFlags.FLAGS_NONE
+            flags=Gio.ApplicationFlags.FLAGS_NONE,
         )
 
         self.window = None
