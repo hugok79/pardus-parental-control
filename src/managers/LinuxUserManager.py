d611d1e7a15c4610a2d07eb653fba99bc60cea00
Emin Fedar <emin.fedar@pardus.org.tr>
2025-01-16 18:12:50 +0300

    fix: improved user session detection

--- src/managers/LinuxUserManager.py
+++ src/managers/LinuxUserManager.py
@@ -1,8 +1,4 @@
 import gi
-import subprocess
-import pwd
-import os
-import managers.FileRestrictionManager as FileRestrictionManager
 
 gi.require_version("AccountsService", "1.0")
 from gi.repository import AccountsService  # noqa
@@ -10,27 +6,12 @@ from gi.repository import AccountsService  # noqa
 manager = AccountsService.UserManager.get_default()
 
 
-def get_logged_username():
-    return pwd.getpwuid(os.getuid()).pw_name
-
-
-def _get_users():
-    list_users = pwd.getpwall()
-
-    return list(filter(lambda x: "bash" in x.pw_shell, list_users))
-
-
-def get_active_session_username():
-    users = _get_users()
+def get_active_session_user():
+    users = manager.list_users()
 
     for u in users:
-        process = subprocess.run(
-            ["loginctl", "show-user", u.pw_name, "-p", "State"], capture_output=True
-        )
-
-        if process.returncode == 0:
-            if "=active" in process.stdout.decode():
-                return u.pw_name
+        if u.get_session() != "":
+            return u
 
     print("Couldnt find active session username from loginctl.")
 
