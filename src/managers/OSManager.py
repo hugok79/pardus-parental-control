153567a310fe74a843b17b81f55a145531bef27b
Emin Fedar <emin.fedar@pardus.org.tr>
2025-10-10 11:49:20 +0300

    fix: OSManager os_codename newline remove fix

--- src/managers/OSManager.py
+++ src/managers/OSManager.py
@@ -2,6 +2,6 @@ def get_os_codename():
     with open("/etc/os-release", "rt") as f:
         for line in f.readlines():
             if "VERSION_CODENAME=" in line:
-                return line.split("=")[1]
+                return line.split("=")[1].strip()
 
     return ""
